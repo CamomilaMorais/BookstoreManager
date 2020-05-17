1e7e5dabcf340d48e003458d8085e56c54424ec5
camila.morais <camila.rmorais73@gmail.com>
2020-05-17 15:52:43 -0300

    Criação de testes unitários para o serviço de find by ID.

---
+++ src/test/java/com/camomila/bookstoremanager/service/BookServiceTest.java
@@ -0,0 +1,41 @@
+package com.camomila.bookstoremanager.service;
+
+import com.camomila.bookstoremanager.dto.BookDTO;
+import com.camomila.bookstoremanager.entity.Book;
+import com.camomila.bookstoremanager.repository.BookRepository;
+import com.camomila.bookstoremanager.utils.BookUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class BookServiceTest {
+
+    @Mock
+    private BookRepository bookRepository;
+
+    @InjectMocks
+    private BookService bookService;
+
+    @Test
+    void whenGivenExistingIdThenReturnThisBook(){
+        Book expectedFoundBook = BookUtils.createFakeBook();
+
+        when(bookRepository.findById(expectedFoundBook.getId())).thenReturn(Optional.of(expectedFoundBook));
+
+        BookDTO bookDTO = bookService.findById(expectedFoundBook.getId());
+
+        assertEquals(expectedFoundBook.getName(), bookDTO.getName());
+        assertEquals(expectedFoundBook.getIsbn(), bookDTO.getIsbn());
+        assertEquals(expectedFoundBook.getPublisherName(), bookDTO.getPublisherName());
+    }
+}
